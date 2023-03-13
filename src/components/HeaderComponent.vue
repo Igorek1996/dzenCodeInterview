56f3a05fd529a9ef2824d1f06646617bb4528aeb
Ihor <ihor96mashtaler@gmail.com>
2023-03-13 16:54:25 +0200

    header finished

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>
-        <h1>header</h1>
-    </div>
+    <header class="header">
+        <div class="container">
+            <div class="header__container">
+                <router-link to="/" class="header__logo">
+                    <img src="../assets/logo.png" alt="logo">
+                </router-link>
+                <search-component />
+                <date-component />
+            </div>
+        </div>
+    </header>
 </template>
 
-<script setup>
-
+<script>
+import SearchComponent from '@/components/SearchComponent.vue';
+import DateComponent from '@/components/DateComponent.vue';
+export default {
+    components: {
+        SearchComponent,
+        DateComponent
+    }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.header {
+    box-shadow: 0 10px 20px rgba($color: #000000, $alpha: 0.5);
+
+    &__container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    &__logo {
+        max-width: 50px;
+    }
+}
+</style>
